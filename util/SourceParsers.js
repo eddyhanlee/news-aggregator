d5262f1305ad87fcd2295196a61e5512bb93d818
dchetian <dan_chetian@intuit.com>
2020-10-21 01:01:18 -0700

    small fix, still need to address DRY violation

--- util/SourceParsers.js
+++ util/SourceParsers.js
@@ -2,8 +2,7 @@ const cheerio = require('cheerio');
 const HelperFunctions = require('./HelperFunctions');
 
 function Parsers() {
-    // future initializations
-    this.helperFunctions = new HelperFunctions();
+    // TODO: initialize helperFunctions to prevent repeat
 }
 
 /*
@@ -19,7 +18,7 @@ Parsers.prototype.cnn = function(source, page) {
     const altHeadline = $('.cd--has-banner .cd__headline-text').first().text();
     const image = $('.media__image').attr('src');
     const link = $('.media').find('a').attr('href');
-    return helperFunctions.formatScrapedData(source, headline, altHeadline, image, link);
+    return this.helperFunctions.formatScrapedData(source, headline, altHeadline, image, link);
 };
 
 /*
@@ -30,13 +29,12 @@ Parsers.prototype.cnn = function(source, page) {
 
  */
 Parsers.prototype.foxnews = function(source, page) {
-    const helperFunctions = new HelperFunctions();
     const $ = cheerio.load(page);
     const headline = $('.collection-spotlight').first().find('h2.title a').text();
     const altHeadline = $('.kicker-text').first().text();
     const image = $('article.story-1 picture').find('img').first().attr('src');
     const link = $('article.story-1').find('a').attr('href');
-    return helperFunctions.formatScrapedData(source, headline, altHeadline, image, link);
+    return this.helperFunctions.formatScrapedData(source, headline, altHeadline, image, link);
 };
 
 module.exports = Parsers;
